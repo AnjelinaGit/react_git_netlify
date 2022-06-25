147dc2e2f7ae186ed4730bc01ea1f20a27d9ed09
AnjelinaGit <101161259+AnjelinaGit@users.noreply.github.com>
2022-06-25 17:24:26 +1000

    Added the Weather contents to React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <div className="Weather-App_Wrapper">
+          <div className="Weather-App">
+            <form className="mb-3">
+              <div class="row">
+                <div className="search_engine">
+                  <input
+                    type="search"
+                    placeholder="Search City"
+                    class="form-control"
+                    autoComplete="off"
+                  />
+
+                  <input
+                    type="Submit"
+                    value="Search"
+                    className="btn btn-primary w-100"
+                  />
+                </div>
+              </div>
+            </form>
+
+            <hr />
+            <div class="overview">
+              <h1> Sydney</h1>
+              <ul>
+                <li>Friday 05:00</li>
+                <li>Clear</li>
+              </ul>
+            </div>
+            <div className="row">
+              <div className="col-8">
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+                  alt="Cloudy"
+                  className="float-left"
+                />
+
+                <div className="float-left">
+                  <strong>12</strong>
+
+                  <span class="units">
+                    {" "}
+                    °C | <a href="/"> °F </a>
+                  </span>
+                </div>
+              </div>
+              <div className="col-4">
+                <Weather />
+              </div>
+              <div className="footer">
+                <small>
+                  <a
+                    href="https://github.com/AnjelinaGit/vanilla-weather-app"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Open-source code_
+                  </a>
+                  by Anjelina Kabul
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <ul>
+        <li>Precitipation:0%</li>
+        <li>Humidity:73%</li>
+        <li>Wind:2 km/h</li>
+      </ul>
+    </div>
+  );
+}
